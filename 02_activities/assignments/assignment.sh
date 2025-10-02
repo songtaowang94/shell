e8544b3fbc719c10dfb48d267ac7ef8fb78beb8f
Steve <songtao.wang@mail.utoronto.ca>
2025-10-02 17:20:22 -0400

    ass1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,6 +38,7 @@ mkdir data
  cp data/raw/*server*.log data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
  cp data/raw/*user*.log data/processed/user_logs/
+ cp data/raw/*event*.log data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
  rm data/raw/*ipaddr*.log
  rm data/processed/user_logs/*ipaddr*.log
